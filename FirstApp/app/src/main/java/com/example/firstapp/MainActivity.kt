b04d79f65ff7a8e43e92a30483172b3c9fa6d5ac
daniel <d.danielprince2003@gmail.com>
2024-05-23 10:10:57 +0530

    Shared Preferences Completed

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.firstapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,16 +17,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var share : SharedPreferences
+    private lateinit var static: String
+    private lateinit var button2 : Button
+    private lateinit var editor : SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        share = getSharedPreferences("MyData" , MODE_PRIVATE)
+        editor = share.edit()
+
+        Log.i("Life" , "Activity Created")
         setContentView(R.layout.activity_main)
         var button1 = findViewById<Button>(R.id.button)
-        var button2 = findViewById<Button>(R.id.button2)
+        button2 = findViewById<Button>(R.id.button2)
         button2.visibility = View.INVISIBLE
         var textView = findViewById<TextView>(R.id.textView)
         var inputField = findViewById<EditText>(R.id.editTextText)
-        var static = ""
+        static = ""
+
         button1.setOnClickListener{
             if(!inputField.text.trim().isEmpty())
             {
@@ -40,5 +50,42 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("NAME" , static.trim())
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Life" , "Activity Started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        static = share.getString("Name" , "Guest").toString()
+        var name = findViewById<TextView>(R.id.textView)
+        name.setText("Welcome $static")
+        if(static.isNotEmpty())
+            button2.visibility = View.VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Life" , "Activity Paused")
+        editor.apply{
+            putString("Name" , static)
+            commit()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Life" , "Activity Stopped")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Life" , "Activity Restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Life" , "Activity Destroy")
     }
 }
--- FirstApp/app/src/main/java/com/example/firstapp/SecondActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,5 +17,33 @@ class SecondActivity : AppCompatActivity() {
         var getName = intent.getStringExtra("NAME")
         val textView = findViewById<TextView>(R.id.textView2)
         textView.text = "Hey, ${getName} ! You got 1 month Free trial"
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Life" , "Activity 2 Started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Life" , "Activity 2 Resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Life" , "Activity 2 Paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Life" , "Activity 2 Stopped")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Life" , "Activity 2 Restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Life" , "Activity 2 Destroy")
     }
 }

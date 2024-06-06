48f5d5cdf5733ac8d0988692ce42e92803acafd4
daniel <d.danielprince2003@gmail.com>
2024-06-06 21:10:22 +0530

    feat:Navigation Graph Created

--- NavigationProject/app/build.gradle.kts
+++ NavigationProject/app/build.gradle.kts
@@ -1,19 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-    kotlin("kapt")
+    id("androidx.navigation.safeargs")
 }
 
 android {
-
-    namespace = "com.example.navigationapp"
+    namespace = "com.example.navigationproject"
     compileSdk = 34
-    viewBinding {
-        enable = true
-    }
+
     defaultConfig {
-        applicationId = "com.example.navigationapp"
+        applicationId = "com.example.navigationproject"
         minSdk = 27
         targetSdk = 34
         versionCode = 1
@@ -41,16 +37,12 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.7.7"
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    kapt("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- NavigationProject/app/src/main/java/com/example/navigationproject/MainActivity.kt
+++ NavigationProject/app/src/main/java/com/example/navigationproject/MainActivity.kt
@@ -1,18 +1,15 @@
-package com.example.navigationapp
+package com.example.navigationproject
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.navigationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
     }
 }

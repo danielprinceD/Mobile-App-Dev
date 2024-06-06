ff981a7b9bd1bc1caf43c417b6c393fe76b30967
daniel <d.danielprince2003@gmail.com>
2024-06-06 09:27:13 +0530

    feat:Query Parameter Passing Method Implemented

--- RetrofitProject/app/src/main/java/com/example/retrofitproject/MainActivity.kt
+++ RetrofitProject/app/src/main/java/com/example/retrofitproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.retrofitproject
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import com.example.retrofitproject.databinding.ActivityMainBinding
 import retrofit2.Response
+import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -18,20 +20,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getRequestFormService()
+    }
+    private fun getRequestFormService(){
         val serviceInstance = RetrofitInstance.getInstance().create(RetrofitService::class.java)
         val liveData : LiveData<Response<Data>> = liveData {
-            val response  = serviceInstance.getItems()
+            val response  = serviceInstance.getItemsByUserId(2)
             emit(response)
         }
+
+        val singleData : LiveData<Response<DataItem>> = liveData{
+            val response = serviceInstance.getDataById(10)
+            emit(response)
+        }
+        singleData.observe(this, Observer {
+            val res = it.body()!!
+            Toast.makeText(this,"User -> ${res.userId} \n Body - > ${res.body}",Toast.LENGTH_SHORT).show()
+        })
+
         liveData.observe(this, Observer {
             val items = it.body()?.listIterator()
             if(items!=null){
                 while(items.hasNext()){
                     val curr = items.next()
-                   val result : String = "${curr.id}. {  \n  id : ${curr.id} , \n\n title : ${curr.title} , \n\n body : ${curr.body} \n } \n\n"
+                    val result : String = "${curr.id}. {  \n  id : ${curr.id} , \n\n title : ${curr.title} , \n\n body : ${curr.body} \n\n , userId : ${curr.userId} } \n\n"
                     binding.textView.append(result)
                 }
             }
         })
     }
+
 }
--- RetrofitProject/app/src/main/java/com/example/retrofitproject/RetrofitService.kt
+++ RetrofitProject/app/src/main/java/com/example/retrofitproject/RetrofitService.kt
@@ -2,8 +2,13 @@ package com.example.retrofitproject
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitService {
     @GET("/posts")
     suspend fun getItems() : Response<Data>
+    @GET("/posts") suspend fun getItemsByUserId(@Query("userId") userId : Int) : Response<Data>
+
+    @GET("/posts/{id}") suspend fun getDataById(@Path("id") id : Int) : Response<DataItem>
 }

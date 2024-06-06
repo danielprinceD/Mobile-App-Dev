79fce990d7032b5812d030b97d9f28319fb41490
daniel <d.danielprince2003@gmail.com>
2024-06-06 21:52:24 +0530

    feat:Navigation for all Fragment has been completed

--- NavigationApp/app/src/main/java/com/example/navigationapp/PasswordPage.kt
+++ NavigationApp/app/src/main/java/com/example/navigationapp/PasswordPage.kt
@@ -5,55 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.navigationapp.databinding.FragmentPasswordPageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PasswordPage.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PasswordPage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentPasswordPageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_page, container, false)
+        binding = FragmentPasswordPageBinding.inflate(layoutInflater)
+        binding.apply {
+            submit.setOnClickListener { passwordChecker(it) }
+        }
+        return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PasswordPage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PasswordPage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun passwordChecker(it : View) = if(binding.password.text.toString() == (binding.confirm.text.toString())){
+        it.findNavController().navigate(R.id.action_passwordPage_to_greetingPage)
+    }else{
+        Toaster.toast(binding.root.context, "Mismatch Password")
     }
 }
